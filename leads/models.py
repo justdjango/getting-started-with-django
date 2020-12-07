4496d326996a1a2d22c951b971e1a48de1fe72b6
mattfreire <freire1996@gmail.com>
2020-12-07 20:03:37 +0200

    Lead model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
