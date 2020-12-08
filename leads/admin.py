af979d88fd630055e24610458224ff99d7667430
mattfreire <freire1996@gmail.com>
2020-12-08 15:51:33 +0200

    Registered models in the admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Lead, Agent
+
+
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
