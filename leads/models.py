3afc03430feef55bb4fd28da7b80a7cc56d4b4c9
mattfreire <freire1996@gmail.com>
2020-12-08 15:35:15 +0200

    Added __str__ methods

--- leads/models.py
+++ leads/models.py
@@ -12,6 +12,11 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
