566e4cee535235f630a9f2c8d84a153b1fd1b099
mattfreire <freire1996@gmail.com>
2020-12-09 16:21:17 +0200

    Added tailwindcss

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from leads.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing-page'),
     path('leads/',  include('leads.urls', namespace="leads"))
 ]
--- leads/views.py
+++ leads/views.py
@@ -4,6 +4,10 @@ from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
 
+def landing_page(request):
+    return render(request, "landing.html")
+
+
 def lead_list(request):
     leads = Lead.objects.all()
     context = {
