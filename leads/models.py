afbf3e1980f3351ab63203f66dd23f96365d5e0f
mattfreire <freire1996@gmail.com>
2020-12-15 14:45:53 +0200

    Post save signal

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
@@ -29,3 +30,11 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.user.email
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(post_user_created_signal, sender=User)
