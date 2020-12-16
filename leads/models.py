e6c0830faa9bf4a80ee977134a682868d410a40b
mattfreire <freire1996@gmail.com>
2020-12-16 13:06:49 +0200

    Leads queryset and optional agent

--- leads/models.py
+++ leads/models.py
@@ -19,7 +19,8 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
