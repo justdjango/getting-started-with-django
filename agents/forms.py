c5f8e2782b1bf5906b389622ac4d6af575c827f3
mattfreire <freire1996@gmail.com>
2020-12-16 13:26:30 +0200

    Create and invite agents

--- agents/forms.py
+++ agents/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-from leads.models import Agent
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
 
 
 class AgentModelForm(forms.ModelForm):
     class Meta:
-        model = Agent
+        model = User
         fields = (
-            'user',
+            'email',
+            'username',
+            'first_name',
+            'last_name'
         )
--- agents/views.py
+++ agents/views.py
@@ -1,3 +1,6 @@
+import random
+
+from django.core.mail import send_mail
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import reverse
@@ -22,9 +25,21 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         return reverse("agents:agent-list")
 
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organisation = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organisor = False
+        user.set_password(f"{random.randint(0, 1000000)}")
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organisation=self.request.user.userprofile
+        )
+        send_mail(
+            subject="You are invited to be an agent",
+            message="You were added as an agent on DJCRM. Please come login to start working.",
+            from_email="admin@test.com",
+            recipient_list=[user.email]
+        )
         return super(AgentCreateView, self).form_valid(form)
 
 
