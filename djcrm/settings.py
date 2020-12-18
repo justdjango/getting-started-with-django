a1c60bb99278962dccaf38f412700761c4077415
mattfreire <freire1996@gmail.com>
2020-12-18 11:42:40 +0200

    Setup postgres db

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'djcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
