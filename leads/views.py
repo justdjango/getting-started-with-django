d08f6b61e254800f87bcfc9d35ae67d086903608
mattfreire <freire1996@gmail.com>
2021-01-05 20:16:02 +0200

    Message when creating a lead

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -118,6 +119,7 @@ class LeadCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
             from_email="test@test.com",
             recipient_list=["test2@test.com"]
         )
+        messages.success(self.request, "You have successfully created a lead")
         return super(LeadCreateView, self).form_valid(form)
 
 
