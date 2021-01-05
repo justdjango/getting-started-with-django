5d3f8ba9eb2b31012f43cb8ec2399a8cfd04c99d
mattfreire <freire1996@gmail.com>
2021-01-05 17:29:32 +0200

    LeadModelAdmin

--- leads/admin.py
+++ leads/admin.py
@@ -3,8 +3,23 @@ from django.contrib import admin
 from .models import User, Lead, Agent, UserProfile, Category
 
 
+
+class LeadAdmin(admin.ModelAdmin):
+    # fields = (
+    #     'first_name',
+    #     'last_name',
+    # )
+
+    list_display = ['first_name', 'last_name', 'age', 'email']
+    list_display_links = ['first_name']
+    list_editable = ['last_name']
+    list_filter = ['category']
+    search_fields = ['first_name', 'last_name', 'email']
+
+
+
 admin.site.register(Category)
 admin.site.register(User)
 admin.site.register(UserProfile)
-admin.site.register(Lead)
+admin.site.register(Lead, LeadAdmin)
 admin.site.register(Agent)
