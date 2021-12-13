c224479f3ef8d404d13259810bcf72728342974b
Shazeb <shazsec0@gmail.com>
2021-12-13 18:27:47 +0530

    Age negative issue

--- leads/models.py
+++ leads/models.py
@@ -23,7 +23,7 @@ class LeadManager(models.Manager):
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    age = models.IntegerField(default=0)
+    age = models.PositiveSmallIntegerField(null=True)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
     category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
